e124917022b6d6ed6f957cb57507cb0b2a13fd46
AL40F13 <soriano11.ans@gmail.com>
2015-05-19 13:56:26 -0500

    uno
    archivos

---
+++ js/acciones.js
@@ -0,0 +1,20 @@
+// JavaScript Document
+$(document).ready(function(e) {
+//document.addEventListener("deviceready",function(){
+	{
+		$('#uno').on('click', function(){
+			$('#pulso').append('1')
+		});
+		$('#dos').on('click', function(){
+			$('#pulso').append('2')
+		});
+		$('#tres').on('click', function(){
+			$('#pulso').append('3')
+		});
+		$('#cuatro').on('click', function(){
+			$('#pulso').append('4')
+		});
+	}
+}); 
+
+
